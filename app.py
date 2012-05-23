e0c0c5ba8d8b10ade6b46c61d9ee1688ce1aaf03
Arjun Vasan <arjun.vasan@gmail.com>
2012-05-23 11:53:31 -0700

    test

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ manager.setup(app)
 
 @app.route('/')
 def hello():
-    document = dict(title="Jantas Restaurant", content="Hello, world!")
-    g.couch["Janta's"] = document
-    return 'Hello World!'
-    
+	document = dict(title="Jantas Restaurant", content="Hello, world!")
+	g.couch["Janta's"] = document
+	return 'Hello World!'
+
 @app.route('/get')
 def get():
-    document = g.couch.get("Janta's") 
-    print document
+	document = g.couch.get("Janta's") 
+	print document
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
