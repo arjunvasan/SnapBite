5986ab54d64004e531ba9274a88e74144e65f6a6
Arjun Vasan <arjun.vasan@gmail.com>
2012-05-23 12:16:00 -0700

    testing

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ app.config["COUCHDB_DATABASE"] = "restaurants"
 from flaskext.couchdb import *
 
 
-manager = CouchDBManager()
+manager = CouchDBManager(auto_sync=False)
 # ...add document types and view definitions...
 manager.setup(app)
+manager.sync(app)
 
 @app.route('/')
 def hello():
 	document = dict(title="Jantas Restaurant", content="Hello, world!")
-	g.couch["jantas"] = document
+	manager.add_document(document)
 	return 'Hello World!'
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)x
