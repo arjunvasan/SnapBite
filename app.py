d951e97e2a1b6bfbc010f111f65caa79455955ee
Arjun Vasan <arjun.vasan@gmail.com>
2012-05-23 11:12:12 -0700

    adding couchdb

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["COUCHDB_SERVER"] = "https://app4790148.heroku:WgINDWRcjikDuH4K1bH7wFPg@app4790148.heroku.cloudant.com"
 app.config["COUCHDB_DATABASE"] = "restaurants"
 
-from flaskext.couchdb import *
+from flaskext import *
 
 manager = CouchDBManager()
 # ...add document types and view definitions...
