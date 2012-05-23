5815283368f4aa398347fa725f51c078fda93415
Arjun Vasan <arjun.vasan@gmail.com>
2012-05-23 11:01:35 -0700

    adding couchdb

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
 # configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+
 
 app = Flask(__name__)
+app.config["COUCHDB_SERVER"] = "https://app4790148.heroku:WgINDWRcjikDuH4K1bH7wFPg@app4790148.heroku.cloudant.com"
+app.config["COUCHDB_DATABASE"] = "restaurants"
+
+from flaskext.couchdb import *
+
+manager = CouchDBManager()
+# ...add document types and view definitions...
+manager.setup(app)
 
 @app.route('/')
 def hello():
+	document = dict(title="Jantas Restaurant", content="Hello, world!")
+	g.couch["Janta's"] = document
     return 'Hello World!'
 
 if __name__ == '__main__':
