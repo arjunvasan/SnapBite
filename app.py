ef0eb9d1eff82e65edf3f4426259e0f5cd1bc254
Arjun Vasan <arjun.vasan@gmail.com>
2012-05-24 09:09:11 -0700

    test

--- app.py
+++ app.py
@@ -16,6 +16,83 @@ manager = CouchDBManager(auto_sync=False)
 manager.setup(app)
 manager.sync(app)
 
+
+from struct import pack
+from OpenSSL import SSL
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory, Protocol
+from twisted.internet.ssl import ClientContextFactory
+import binascii
+import struct
+
+APNS_SERVER_HOSTNAME = "gateway.sandbox.push.apple.com"
+APNS_SERVER_PORT = 2195
+APNS_SSL_CERTIFICATE_FILE = "<your ssl certificate.pem>"
+APNS_SSL_PRIVATE_KEY_FILE = "<your ssl private key.pem>"
+DEVICE_TOKEN = "<hexlified device token>"
+MESSAGE = '{"aps":{"alert":"twisted test"}}'
+
+
+API_FORMAT = """
+
+  # = [ORDER] Call URL =======================
+  # ==========================================
+
+  # http://snapbite.heroku.com/device_comm/order
+	
+  # = [ORDER] STANDARD PARAMETER FORMATS =====
+  # ==========================================
+
+  # restaurant_id     => 'SOME_RESTAURANT_ID'                                           (String)   
+  # table             => '2'                                                            (String)   
+  # dish              => 'Eggplant Parmesan'                                            (String)   
+  # special           => 'Substitute marinara for the alfredo sauce on the pasta'       (String)
+
+  # ==========================================
+  # = DEFINE STANDARD RESPONSE OBJECT FORMAT =
+  # ==========================================
+
+  # status_code        => 200 (success) or 400 (failure)
+
+	
+
+"""
+
+class APNSClientContextFactory(ClientContextFactory):
+	def __init__(self):
+		self.ctx = SSL.Context(SSL.SSLv3_METHOD)
+		self.ctx.use_certificate_file(APNS_SSL_CERTIFICATE_FILE)
+		self.ctx.use_privatekey_file(APNS_SSL_PRIVATE_KEY_FILE)
+
+	def getContext(self):
+		return self.ctx
+
+class APNSProtocol(Protocol):
+
+	def connectionMade(self):
+		print "connection made"
+		self.sendMessage(binascii.unhexlify(DEVICE_TOKEN), MESSAGE)
+		self.transport.loseConnection()
+
+	def sendMessage(self, deviceToken, payload):
+		fmt = "!cH32sH%ds" % len(payload)
+		command = '\x00'
+		msg = struct.pack(fmt, command, 32, deviceToken,len(payload), payload)
+		print "%s: %s" %(binascii.hexlify(deviceToken), binascii.hexlify(msg))
+		self.transport.write(msg)
+
+class APNSClientFactory(ClientFactory):
+	def buildProtocol(self, addr):
+		print "Connected to APNS Server %s:%u" % (addr.host, addr.port)
+		return APNSProtocol()
+
+	def clientConnectionLost(self, connector, reason):
+		print "Lost connection. Reason: %s" % reason
+
+	def clientConnectionFailed(self, connector, reason):
+		print "Connection failed. Reason: %s" % reason
+
+
 @app.route('/device_comm/alert')
 def alert():
 	return "call waiter"
@@ -29,7 +106,6 @@ def order():
 	)
 	
 	post.store()
-	
 	
 	return "order food"
 	
